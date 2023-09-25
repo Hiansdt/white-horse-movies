b235d24fc36a11587e6b20fd8a240cf8bcd3f909
Hiansdt <vinijao123690@gmail.com>
2023-09-25 17:02:04 -0300

    naodeutempopraescrever

--- src/views/SadView.vue
+++ src/views/SadView.vue
@@ -30,47 +30,70 @@ onMounted(() => {
 
 <template>
     <main>
-        <h1 v-if="quote">
-            Here's a quote from {{ quote[0].author }} to hype you up:
-        </h1>
-        <div v-if="quote" class="quote">
-            <p>" {{ quote[0].quote }} " </p>
-        </div>
-        <h1 v-if="quote" class="recommendation">
-            And here's a movie recommendation for you:
-        </h1>
-        <div class="movie" v-if="movieImages">
-            <img :src="'https://image.tmdb.org/t/p/original' + movie.poster_path" alt="">
-            <div class="movieInfo">
-                <h2 class="title">{{ movie.title }}</h2>
-                <p>{{ movie.overview }}</p>
+        <div class="quoteContainer">
+            <h1 v-if="quote">
+                Here's a quote from {{ quote[0].author }} to hype you up:
+            </h1>
+            <div v-if="quote" class="quote">
+                <p>" {{ quote[0].quote }} " </p>
             </div>
         </div>
-        
-        <FooterComponentVue />
+        <hr>
+        <div class="movieContainer">
+            <h1 v-if="quote" class="recommendation">
+                And here's a movie recommendation for you:
+            </h1>
+            <div class="movie" v-if="movieImages">
+                <img :src="'https://image.tmdb.org/t/p/original' + movie.poster_path" alt="">
+                <div class="movieInfo">
+                    <h2 class="title">{{ movie.title }}</h2>
+                    <p>{{ movie.overview }}</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <FooterComponentVue class="footer"/>
     </main>
 </template>
 
 <style scoped>
+
+.quoteContainer {
+    background-color: #005f66;
+    color: white;
+    width: 100%;
+    text-align: center;
+    padding: 1%;
+}
+
+hr {
+    background-color: white;
+    color: white;
+}
+
+.movieContainer {
+    background-color: #005f66;
+    width: 100%;
+    padding: 1%;
+    height: 75vh;
+    text-align: center;
+    color: white;
+}
 
 p {
     font-size: larger;
     text-align: center;
 }
 
-.recommendation {
-    margin-top: 5%;
-}
-
 h2 {
     font-size: xx-large;
-    color: black;
+    color: rgb(255, 255, 255);
     margin-bottom: 10%;
 }
 
 main {
-    width: 100%;
-    height: 100%;
+    width: 99%;
+    height: 99%;
 }
 
 .movie {
@@ -87,7 +110,7 @@ main {
 }
 
 img {
-    width: 350px;
+    width: 415px;
     margin-left: 3%;
 }
 
